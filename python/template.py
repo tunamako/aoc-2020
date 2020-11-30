8b59939ea645d198484143887e2689fddff71179
tunamako <inedibledelicacies@gmail.com>
2020-11-30 17:10:25 -0600

    init

---
+++ python/template.py
@@ -0,0 +1,33 @@
+from aocd import submit
+from aocd.models import Puzzle
+
+import cProfile
+from collections import defaultdict, namedtuple, Counter, deque
+from itertools import permutations, combinations, chain
+import re
+import math
+
+YEAR = 2020
+DAY = 1
+
+def part_one(_input):
+    pass
+
+
+def part_two(_input):
+    pass
+
+
+if __name__ == '__main__':
+    puzzle = Puzzle(year=YEAR, day=DAY)
+    _input = puzzle.input_data.split('\n')
+    #_input = open('bigboy').readlines()
+
+    print(part_one(_input))
+    print(part_two(_input))
+
+    #submit(part_one(_input), part="a", day=DAY, year=YEAR)
+    #submit(part_two(_input), part="b", day=DAY, year=YEAR)
+
+    #cProfile.run('print(part_one(_input))')
+    #cProfile.run('print(part_two(_input))')
