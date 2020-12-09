988e449daecaeedd4657ae43b772e0569404363b
tunamako <inedibledelicacies@gmail.com>
2020-12-09 01:13:09 -0600

    added remaining days

---
+++ python/day21/day21.py
@@ -0,0 +1,31 @@
+from aocd.models import Puzzle
+
+import cProfile
+from collections import defaultdict, namedtuple, Counter, deque
+from itertools import permutations, combinations, chain
+import re
+import math
+
+
+YEAR = 2020
+DAY = 1
+
+
+def part_one(_input):
+    pass
+
+
+def part_two(_input):
+    pass
+
+
+if __name__ == '__main__':
+    puzzle = Puzzle(year=YEAR, day=DAY)
+    _input = puzzle.input_data.split('\n')
+    #_input = open('bigboy').readlines()
+
+    print(part_one(_input))
+    print(part_two(_input))
+
+    #cProfile.run('print(part_one(_input))')
+    #cProfile.run('print(part_two(_input))')
