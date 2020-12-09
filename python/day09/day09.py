d33e0d5bad70657d9f7b4f36ea3e3737147c893f
tunamako <inedibledelicacies@gmail.com>
2020-12-09 01:05:45 -0600

    day9

--- python/day09/day09.py
+++ python/day09/day09.py
@@ -6,22 +6,35 @@ from itertools import permutations, combinations, chain
 import re
 import math
 
-
 YEAR = 2020
-DAY = 1
+DAY = 9
 
 
 def part_one(_input):
-    pass
+    predicate = _input[:25]
+
+    for num in _input[25:]:
+        if num not in set(map(sum, combinations(predicate, 2))):
+            return num
+
+        predicate = predicate[1:] + [num]
 
 
 def part_two(_input):
-    pass
+    invalid = part_one(_input)
+
+    for i in range(len(_input)):
+        acc = 0
+        for j in range(i, len(_input)):
+            acc += _input[j]
+
+            if acc == invalid:
+                return max(_input[i:j+1]) + min(_input[i:j+1])
 
 
 if __name__ == '__main__':
     puzzle = Puzzle(year=YEAR, day=DAY)
-    _input = puzzle.input_data.split('\n')
+    _input = list(map(int, puzzle.input_data.split('\n')))
     #_input = open('bigboy').readlines()
 
     print(part_one(_input))
