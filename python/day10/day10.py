0b03d69b435747443b9baf2e2221f82968aefdea
tunamako <inedibledelicacies@gmail.com>
2020-12-11 00:24:16 -0600

    day11

--- python/day10/day10.py
+++ python/day10/day10.py
@@ -8,20 +8,122 @@ import math
 
 
 YEAR = 2020
-DAY = 1
+DAY = 10
 
 
-def part_one(_input):
-    pass
+test1 = set([
+    16,
+    10,
+    15,
+    5,
+    1,
+    11,
+    7,
+    19,
+    6,
+    12,
+    4,
+])
 
 
-def part_two(_input):
-    pass
+test2 = set([
+    28,
+    33,
+    18,
+    42,
+    31,
+    14,
+    46,
+    20,
+    48,
+    47,
+    24,
+    23,
+    49,
+    45,
+    19,
+    38,
+    39,
+    11,
+    1,
+    32,
+    25,
+    35,
+    8,
+    17,
+    7,
+    9,
+    4,
+    2,
+    34,
+    10,
+    3,
+])
 
+
+def find_chain(adapters, chain=None, cur_joltage=0):
+    if chain is None: chain = []
+
+    if not adapters:
+        return chain
+
+    for i, a in enumerate(adapters):
+
+        if a - 3 <= cur_joltage <= a + 1:
+            adapters.remove(a)
+
+            out = find_chain(adapters, chain + [a], a) 
+            if out is None:
+                adapters.add(a)
+            else:
+                return out
+
+    return None
+
+
+def part_one(adapters):
+    adapters = adapters.copy()
+    rating =  max(adapters) + 3
+
+    chain = find_chain(adapters)
+
+    onediffs = 0
+    threediffs = 1
+
+    if chain[0] == 1:
+        onediffs += 1
+    elif chain[0] == 3:
+        threediffs += 1
+
+    for i in range(len(chain) - 1):
+        if abs(chain[i + 1] - chain[i]) == 1:
+            onediffs += 1
+        elif abs(chain[i + 1] - chain[i]) == 3:
+            threediffs += 1
+
+    return onediffs * threediffs
+
+
+def part_two(adapters):
+    adapters = sorted(list(adapters))
+
+    step_counts = dict()
+    step_counts[0] = 1
+
+    for a in adapters:
+        step_counts[a] = 0
+        if a - 3 in step_counts:
+            step_counts[a] += step_counts[a - 3]
+        if a - 2 in step_counts:
+            step_counts[a] += step_counts[a - 2]
+        if a - 1 in step_counts:
+            step_counts[a] += step_counts[a - 1]
+
+    return(step_counts[max(adapters)])
 
 if __name__ == '__main__':
     puzzle = Puzzle(year=YEAR, day=DAY)
-    _input = puzzle.input_data.split('\n')
+    _input = set(map(int, puzzle.input_data.split('\n')))
     #_input = open('bigboy').readlines()
 
     print(part_one(_input))
--- python/day11/day11.py
+++ python/day11/day11.py
@@ -5,27 +5,97 @@ from collections import defaultdict, namedtuple, Counter, deque
 from itertools import permutations, combinations, chain
 import re
 import math
+import numpy as np
+import sys
 
+np.set_printoptions(threshold=sys.maxsize)
 
 YEAR = 2020
-DAY = 1
+DAY = 11
 
+DIRECTIONS = list(permutations([-1, 0, 1], 2)) + [(1, 1), (-1, -1)]
+SHAPE = None
+def get_neighbors_1(x, y, grid):
+    count = 0
+
+    for d in DIRECTIONS:
+        x_delta = x+d[0]
+        y_delta = y+d[1]
+
+        if (0 <=x_delta < SHAPE[0]) and (0 <=y_delta < SHAPE[1]):
+            if grid[x+d[0]][y+d[1]] == '#':
+                count += 1
+
+    return count
+
+def get_neighbors_2(x, y, grid):
+    count = 0
+
+    for d in DIRECTIONS:
+        x_delta = x+d[0]
+        y_delta = y+d[1]
+
+        while (0 <=x_delta < SHAPE[0]) and (0 <=y_delta < SHAPE[1]):
+            if grid[x_delta][y_delta] == 'L':
+                break
+            if grid[x_delta][y_delta] == '#':
+                count += 1
+                break
+
+            x_delta += d[0]
+            y_delta += d[1]
+
+
+    return count
+
+def next_gen(grid, neighbor_func, threshold):
+    to_change = []
+
+    for x in range(SHAPE[0]):
+        for y in range(SHAPE[1]):
+            if grid[x][y] == '.':
+                continue
+
+            live_neighbors = neighbor_func(x, y, grid)
+
+            if grid[x][y] == 'L' and live_neighbors == 0:
+                to_change.append((x,y,'#'))
+            elif grid[x][y] == '#' and live_neighbors >= threshold:
+                to_change.append((x,y,'L'))
+
+    for change in to_change:
+        grid[change[0]][change[1]] = change[2]
+
+    return bool(to_change)
 
 def part_one(_input):
-    pass
+    while next_gen(_input, get_neighbors_1, 4):
+        continue
+
+    return np.count_nonzero(_input == '#')
 
 
 def part_two(_input):
-    pass
+    while next_gen(_input, get_neighbors_2, 5):
+        continue
+
+    return np.count_nonzero(_input == '#')
+
+
 
 
 if __name__ == '__main__':
     puzzle = Puzzle(year=YEAR, day=DAY)
-    _input = puzzle.input_data.split('\n')
+    _input = np.array(list(map(list, puzzle.input_data.split('\n'))))
+    #_input = np.array([list(row[:-1]) for row in open('input', 'r').readlines()])
+    _input = np.swapaxes(_input, 0, 1)
+    SHAPE = np.shape(_input)
     #_input = open('bigboy').readlines()
 
-    print(part_one(_input))
-    print(part_two(_input))
+    #print(part_one(_input.copy()))
+    #print(part_two(_input.copy()))
 
-    #cProfile.run('print(part_one(_input))')
+    _input = _input.copy()
+    cProfile.run('print(part_one(_input))')
+    #_input = _input.copy()
     #cProfile.run('print(part_two(_input))')
