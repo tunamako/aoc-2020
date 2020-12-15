42573d2f889bf63d81cdced218c0b39eed6f1daf
tunamako <inedibledelicacies@gmail.com>
2020-12-15 00:25:05 -0600

    day15

--- python/day15/day15.py
+++ python/day15/day15.py
@@ -1,4 +1,4 @@
-from aocd.models import Puzzle
+#from aocd.models import Puzzle
 
 import cProfile
 from collections import defaultdict, namedtuple, Counter, deque
@@ -8,24 +8,43 @@ import math
 
 
 YEAR = 2020
-DAY = 1
+DAY = 15
 
+
+def play_gaem(array, cap):
+    spoken = dict()
+    last = 0
+
+    for i, num in enumerate(array):
+        spoken[num] = i + 1
+
+    for i in range(len(array) + 1, cap):
+        if last in spoken:
+            tmp = i - spoken[last]
+            spoken[last] = i
+            last = tmp
+        else:
+            spoken[last] = i
+            last = 0
+
+    return last
 
 def part_one(_input):
-    pass
-
+    _input = [int(i) for i in _input.split(',')]
+    return play_gaem(_input, 2020)
 
 def part_two(_input):
-    pass
+    _input = [int(i) for i in _input.split(',')]
+    return play_gaem(_input, 30000000)
 
 
 if __name__ == '__main__':
-    puzzle = Puzzle(year=YEAR, day=DAY)
-    _input = puzzle.input_data.split('\n')
+    #_input = "6,19,0,5,7,13,1"
+    _input = "0,3,6"
     #_input = open('bigboy').readlines()
 
-    print(part_one(_input))
-    print(part_two(_input))
+    #print(part_one(_input))
+    #print(part_two(_input))
 
-    #cProfile.run('print(part_one(_input))')
-    #cProfile.run('print(part_two(_input))')
+    cProfile.run('print(part_one(_input))')
+    cProfile.run('print(part_two(_input))')
